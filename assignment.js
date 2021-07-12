4cff3f9236e4a239f45972b27b12e3819da8303f
Ahmed Saad Rupai <saadrupai1997@gmail.com>
2021-07-13 04:24:21 +0600

    adding third function to the file

--- assignment.js
+++ assignment.js
@@ -20,3 +20,37 @@ function budgetCalculator(watch, mobile, laptop)
 // var result=budgetCalculator(2,2,2);
 // console.log(result);
 
+
+function hotelCost(numberOfDays)
+{
+    var days=Math.abs(numberOfDays);
+    var cost=0;
+    if(days<=10)
+    {
+        cost= days*100;
+    }
+    else if(days<=20)
+    {
+        var firstPeriod=10*100;
+        var remaining=days-10;
+        var secondPeriod=remaining*80;
+        cost=firstPeriod+secondPeriod;
+    }
+    else
+    {
+        var firstPeriod=10*100;
+        var secondPeriod=10*80;
+        var remaining=days-20;
+        var thirdPeriod=remaining*50;
+        cost=firstPeriod+secondPeriod+thirdPeriod;
+    }
+    return cost;
+}
+
+// var result=hotelCost(21);
+// console.log(result);
+
+function megaFriend(names)
+{
+    
+}
