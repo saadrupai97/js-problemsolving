0bc6398a37e0e5a7bd584c42facb85fa37a4ec71
Ahmed Saad Rupai <saadrupai1997@gmail.com>
2021-07-13 06:03:35 +0600

    Final submission

--- assignment.js
+++ assignment.js
@@ -20,18 +20,19 @@ function budgetCalculator(watch, mobile, laptop) {
 
 
 function hotelCost(numberOfDays) {
-    var days = Math.abs(numberOfDays);
+
+    var days = Math.abs(numberOfDays);            //changing negative inputs into positive
     var cost = 0;
-    if (days <= 10) {
-        cost = days * 100;
+    if (days <= 10) {                             //first case from day 1 to day 10 where rent is 100
+        cost = days * 100;  
     }
-    else if (days <= 20) {
+    else if (days <= 20) {                        //second case from day 11 to day 20 where rent is 80
         var firstPeriod = 10 * 100;
         var remaining = days - 10;
         var secondPeriod = remaining * 80;
         cost = firstPeriod + secondPeriod;
     }
-    else {
+    else {                                        //third case from day 21 to unlimited where rent is 50
         var firstPeriod = 10 * 100;
         var secondPeriod = 10 * 80;
         var remaining = days - 20;
@@ -45,11 +46,11 @@ function hotelCost(numberOfDays) {
 // console.log(result);
 
 function megaFriend(names) {
-    var max = names[0].length;
-    var maxName=' ';
-    for (var i=0; i<names.length; i++) {
+    var max = names[0].length;                    //taking initial length of first name to compare 
+    var maxName = ' ';
+    for (var i = 0; i < names.length; i++) {
 
-        if (names[i] == ' ') {
+        if (names[i] == ' ') {                    //to avoid wrong whitespace inputs
             continue;
         }
 
@@ -57,11 +58,11 @@ function megaFriend(names) {
             count = names[i].length
             if (count > max) {
                 max = count;
-                maxName=names[i];
+                maxName = names[i];               //storing the name of max length of character count 
             }
         }
     }
-    return maxName;   
+    return maxName;
 }
 
 // var name = [' ', 'gotze', ' ', 'sakibs'];
